d458166273c7b1071f42c702ae235d8f2f8caa73
RingZero <118491337+ringzerocode@users.noreply.github.com>
2022-11-18 22:10:57 +0100

    Add files via upload

---
+++ shell.s
@@ -0,0 +1,15 @@
+global _start
+
+section .text
+_start:
+    mov al, 59          ; execve syscall number
+    xor rdx, rdx        ; set env to NULL
+    push rdx            ; push NULL string terminator
+    mov rdi, '/bin//sh' ; first arg to /bin/sh
+    push rdi            ; push to stack 
+    mov rdi, rsp        ; move pointer to ['/bin//sh']
+    push rdx            ; push NULL string terminator
+    push rdi            ; push second arg to ['/bin//sh']
+    mov rsi, rsp        ; pointer to args
+    syscall
+    
